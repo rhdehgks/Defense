bad26a53f9a7ff20511431bf71d2a9f254c0d735
rhdehgks <rhdehgks@naver.com>
2023-05-16 00:33:58 +0900

    스크린 wrap 추가

--- game_screen/man.gd
+++ game_screen/man.gd
@@ -1,10 +1,14 @@
 extends RigidBody2D
 
 @onready var timer := $Timer
+@onready var screen_size := get_viewport_rect().size
 
 func _ready():
 	timer.wait_time = randf_range(1, 2)
 	timer.start()
+	
+func _physics_process(delta):
+	_screen_wrap()
 
 func _on_timer_timeout():
 	apply_impulse(_get_random_direction() * 100)
@@ -12,3 +16,13 @@ func _on_timer_timeout():
 
 func _get_random_direction():
 	return Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+
+func _screen_wrap():
+	if global_position.x > screen_size.x:
+		global_position.x = 0
+	if global_position.x < 0:
+		global_position.x = screen_size.x
+	if global_position.y > screen_size.y:
+		global_position.y = 0
+	if global_position.y < 0:
+		global_position.y = screen_size.y
