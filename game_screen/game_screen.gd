3665467687b916ba3a227a8e2bd932db9e3e413d
rhdehgks <rhdehgks@naver.com>
2023-05-15 23:33:03 +0900

    폰트 추가

--- game_screen/game_screen.gd
+++ game_screen/game_screen.gd
@@ -1,8 +1,10 @@
 extends Control
 
+@onready var fps_label := $FPSLabel
+
 func _ready():
-	size = get_viewport_rect().size
-	
-func _process(delta):
 	if size != get_viewport_rect().size:
 		size = get_viewport_rect().size
+	
+func _process(delta):
+	fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
