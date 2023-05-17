d00ae3db3efbe2b28d9bfe6bcc0aca29e5051cb4
rhdehgks <rhdehgks@naver.com>
2023-05-17 23:34:37 +0900

    특수 조명 추가

--- game_screen/astroneer.gd
+++ game_screen/astroneer.gd
@@ -2,7 +2,12 @@ extends RigidBody2D
 
 var direction := Vector2(0, 0)
 var speed := 800
-var jump_strength := 400
+var jump_strength := 600
+
+var is_up := false
+var up_strength := 200
+
+@onready var particles := $GPUParticles2D
 
 func _process(delta):
 	direction = Vector2(
@@ -11,6 +16,19 @@ func _process(delta):
 	).normalized()
 	if Input.is_action_just_pressed("ui_up"):
 		apply_impulse(Vector2(0, -jump_strength))
+		particles.emitting = true
+	if Input.is_action_pressed("ui_up"):
+		particles.emitting = true
+		is_up = true
+	else:
+		particles.emitting = false
+		is_up = false
+		up_strength = 200
 	
 func _physics_process(delta):
 	apply_force(direction * speed)
+	if is_up:
+		up_strength += 10
+		up_strength = min(2000, up_strength)
+		print(up_strength)
+		apply_force(Vector2(0, -up_strength))
